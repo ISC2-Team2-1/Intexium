9a7aa8229359938ca761ad22fa0ad0e78230af41
Tyler Barty <tyler.barty@gmail.com>
2021-04-07 18:24:52 -0600

    comit indexview

---
+++ Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Intexium.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipes.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
